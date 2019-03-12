436a98009a3c569b36dbb9dfece67221afac1c9e
lisw4y <lsw99u@gmail.com>
2019-03-12 16:36:07 -0700

    Adopt express.js and create several simple static pages

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const http = require('http');
-const routes = require('./routes');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-// Alternative 1
-// const server = http.createServer(routes);
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Alternative 2
-const server = http.createServer(routes.handler);
-console.log(routes.text);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-server.listen(3000);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
