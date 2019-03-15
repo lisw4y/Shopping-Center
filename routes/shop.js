60cbfe8fdff44a85cd4281302f5c956f1549df09
lisw4y <lsw99u@gmail.com>
2019-03-14 21:38:21 -0700

    Adopt pug templating engine and add css styles to forms

--- routes/shop.js
+++ routes/shop.js
@@ -3,9 +3,14 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {
+    pageTitle: 'My Shop',
+    products: adminData.products,
+    path: '/'
+  });
 });
 
 module.exports = router;
